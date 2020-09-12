a1afd1bee283c2d484c38b2d1add96c535f95eb4
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2020-09-12 17:17:32 +0530

    new_commit

--- homeapp/views.py
+++ homeapp/views.py
@@ -5,43 +5,47 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 import math
-from math import cos, sqrt
-R = 6371000 #radius of the Earth in m
-from math import radians, cos, sin, asin, sqrt 
-def distance(lat1, lon1, lat2, lon2):
-    # The math module contains a function named 
-    # radians which converts from degrees to radians. 
-    lon1 = radians(lon1) 
-    lon2 = radians(lon2) 
-    lat1 = radians(lat1) 
-    lat2 = radians(lat2) 
+# from math import cos, sqrt
+# R = 6371000 #radius of the Earth in m
+# from math import radians, cos, sin, asin, sqrt 
+# def distance(lat1, lon1, lat2, lon2):
+#     # The math module contains a function named 
+#     # radians which converts from degrees to radians. 
+#     lon1 = radians(lon1) 
+#     lon2 = radians(lon2) 
+#     lat1 = radians(lat1) 
+#     lat2 = radians(lat2) 
     
-    # Haversine formula  
-    dlon = lon2 - lon1  
-    dlat = lat2 - lat1 
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+#     # Haversine formula  
+#     dlon = lon2 - lon1  
+#     dlat = lat2 - lat1 
+#     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
 
-    c = 2 * asin(sqrt(a))  
+#     c = 2 * asin(sqrt(a))  
     
-    # Radius of earth in kilometers. Use 3956 for miles 
-    r = 6371
+#     # Radius of earth in kilometers. Use 3956 for miles 
+#     r = 6371
     
-    # calculate the result 
-    return(c * r) 
+#     # calculate the result 
+#     return(c * r) 
 @login_required
 def category(request):
-    if 'getByCoordinate' in request.POST:
-        latitude = int(request.POST['latitude'])
-        longitude = int(request.POST['longitude'])
-        radius = int(request.POST['radius'])
-        print(latitude,longitude,radius)
-        all_houses = Item.objects.all()
-        data = []
-        for house in all_houses:
-            if house.latitude and house.longitude and distance(house.latitude,house.longitude,latitude,longitude) <= radius:
-                data.append(house)
-    else:
-        data = Item.objects.all()
+    # if 'getByCoordinate' in request.POST:
+    #     latitude = int(request.POST['latitude'])
+    #     longitude = int(request.POST['longitude'])
+    #     radius = int(request.POST['radius'])
+    #     print(latitude,longitude,radius)
+    #     all_houses = Item.objects.all()
+    #     data = []
+    #     for house in all_houses:
+    #         if house.latitude and house.longitude and distance(house.latitude,house.longitude,latitude,longitude) <= radius:
+    #             data.append(house)
+    # else:
+    data = Item.objects.all()
+    if 'search' in request.POST:
+        city = request.POST['city']
+        data = Item.objects.filter(city__contains = city )
+    
     return render(request,'all_categories.html',{'all_category_objects':data})
 
 def home(request):
@@ -66,11 +70,11 @@ def login(request):#copied frm beat-connect master
     #return render(request, 'login.html')
 def register(request):
     if 'register' in request.POST:
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
+        # first_name = request.POST['first_name']
+        # last_name = request.POST['last_name']
         username = request.POST['username']
-        age = request.POST['age']
-        number =request.POST['phone']
+        # age = request.POST['age']
+        # number =request.POST['phone']
         image =request.FILES['image']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
@@ -78,9 +82,9 @@ def register(request):
             if User.objects.filter(username = username).exists():
                 return render(request,'home.html',{'error_message':"Username already taken"})
             else:
-                user= User.objects.create_user(username = username, password = password1, first_name= first_name, last_name = last_name)
+                user= User.objects.create_user(username = username, password = password1)
                 user.save()
-                my_extend_user = extendedUser(belongs_to= user,phone_number=number,age=age,image=image )
+                my_extend_user = extendedUser(belongs_to= user,image=image )
                 my_extend_user.save()
                 return redirect('/')
         else:
@@ -129,11 +133,11 @@ def addhome(request):
         number =request.POST['phone']
         image =request.FILES['image']
         description =request.POST['description']
-        bedroom1 =request.FILES['bedroom1']
-        bedroom2 =request.FILES['bedroom2']
-        kitchen =request.FILES['kitchen']
-        other =request.FILES['other']
-        new_item= Item(user=request.user,city=city,address=address,rent=rent,number=number,description=description,picture=image,bedroom1=bedroom1,bedroom2=bedroom2,kitchen=kitchen,other=other)
+        # bedroom1 =request.FILES['bedroom1']
+        # bedroom2 =request.FILES['bedroom2']
+        # kitchen =request.FILES['kitchen']
+        # other =request.FILES['other']
+        new_item= Item(user=request.user,city=city,address=address,rent=rent,number=number,description=description,picture=image)
         new_item.save()
         return redirect('/home/')
     return render(request,'addhome.html')
