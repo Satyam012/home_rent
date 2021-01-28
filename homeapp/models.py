cdf1aeceb1ef2e320b013cd2e7042a29f6675516
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2021-01-28 20:30:48 +0530

    change wallpaper

--- homeapp/models.py
+++ homeapp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Image(models.Model):
-    image = models.FileField()
+# class Image(models.Model):
+#     image = models.FileField()
 class Item(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="item", null=True)
     city = models.CharField(max_length=50)
