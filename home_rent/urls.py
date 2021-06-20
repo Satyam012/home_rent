a7c4fb552df9efcdb5f95138dac81e99121c8e00
satyam jaiswal <56363925+Satyam012@users.noreply.github.com>
2021-06-21 00:21:22 +0530

    update

--- home_rent/urls.py
+++ home_rent/urls.py
@@ -19,10 +19,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/',LogoutView.as_view(), name="logout"),
     path('',include('homeapp.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),     
 ]
 if settings.DEBUG: #Not for production code
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
